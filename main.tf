0c1ead2313568baab5c1dda9fcfbafce12ba899d
Miguel Xavier <miguel.xavier@pucrs.br>
2024-09-20 06:08:25 +0000

    Short changes

--- main.tf
+++ main.tf
@@ -1,10 +1,6 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_instance" "example" {
   ami           = "ami-0142b530ec8935389" 
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
 
   tags = {
     Name = "terraform-example-instance"
