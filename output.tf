ae1cd07d15be7d9d83d9b62962e2c22892d13384
Miguel Xavier <miguel.xavier@pucrs.br>
2024-11-26 22:05:01 +0000

    Adds ECS description

---
+++ output.tf
@@ -0,0 +1,27 @@
+output "vpc_id" {
+    value = "${module.vpc.vpc_id}"
+}
+
+output "vpc_cidr_block" {
+    value = "${module.vpc.vpc_cidr_block}"
+}
+
+output "public_subnets" {
+    value = "${module.vpc.public_subnets}"
+}
+
+output "private_subnets" {
+    value = "${module.vpc.private_subnets}"
+}
+
+output "database_subnets" {
+    value = "${module.vpc.database_subnets}"
+}
+
+output "ecs_cluster_name" {
+    value = "${module.ecs_cluster.cluster_name}"
+}
+
+output "ecs_cluster_arn" {
+    value = "${module.ecs_cluster.cluster_arn}"
+}
