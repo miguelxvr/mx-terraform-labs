e6fb20fca294453a888773c0c00e6a7d32028295
Miguel Xavier <miguel.xavier@pucrs.br>
2024-11-26 21:16:25 +0000

    Initial commit

--- version.tf
+++ version.tf
@@ -2,15 +2,4 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform { 
-    backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "PUCRS"
-
-    workspaces {
-      prefix = "csw242-"
-    }
-  }
-}
-
 data "aws_availability_zones" "available" {}
